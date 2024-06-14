ff2f13388d34b3730d8b7e39a5c1757a1cb3be40
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-14 13:38:15 +0200

    feature(add pagination + res.status)

--- controllers/NotificationController.js
+++ controllers/NotificationController.js
@@ -5,13 +5,32 @@ const Notification = require("../models/notification");
 
 router.get("/:userId", async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
     const notifications = await Notification.find({
       user_id: req.params.userId,
+    })
+      .skip(skip)
+      .limit(limit);
+
+    const totalNotifications = await Notification.countDocuments({
+      user_id: req.params.userId,
     });
+
     if (!notifications) {
-      return res.status(404).json({ message: "Notifications not found!" });
+      return res.status(404).json({ message: "Not found" });
     }
-    res.json(notifications);
+
+    if (notifications.length === 0) {
+      return res.status(201).json({ message: "Empty" });
+    }
+
+    res.status(201).json({
+      totalPages: Math.ceil(totalNotifications / limit),
+      notifications: notifications,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
