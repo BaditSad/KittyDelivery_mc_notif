d6a0acdf47c569855491dfcde9376c5a2ae1e2cb
KubArthur <arthur.kubiak@viacesi.fr>
2024-06-20 02:25:19 +0200

    feature(add post, put notification endpoints)

--- controllers/NotificationController.js
+++ controllers/NotificationController.js
@@ -3,6 +3,25 @@ const router = express.Router();
 module.exports = router;
 const Notification = require("../models/notification");
 
+router.post("/", async (req, res) => {
+  try {
+    const { user_id, notification_type, notification_message } = req.body;
+
+    const newNotification = new Notification({
+      user_id,
+      notification_type,
+      notification_message,
+      notification_is_read: false,
+    });
+
+    await newNotification.save();
+
+    res.status(201).json({ message: "Item posted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 router.get("/:userId", async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
@@ -32,6 +51,28 @@ router.get("/:userId", async (req, res) => {
       notifications: notifications,
     });
   } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.put("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params.userId;
+    const { notification_is_read } = req.body;
+
+    const updatedNotification = await Notification.findByIdAndUpdate(
+      userId,
+      { notification_is_read },
+      { new: true }
+    );
+
+    if (!updatedNotification) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    res.status(201).json({ message: "Item updated" });
+  } catch (error) {
+    console.error("Error updating notification:", error);
     res.status(500).json({ message: error.message });
   }
 });
